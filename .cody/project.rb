22b9a37a0f1f3d75268aa0ab1d039452fd2f7ad0
Tung Nguyen <tongueroo@gmail.com>
2020-06-29 22:32:40 +0000

    update buildspec

--- .cody/project.rb
+++ .cody/project.rb
@@ -1,5 +1,6 @@
 github_url("https://github.com/tongueroo/cody-demo.git")
-linux_image("aws/codebuild/amazonlinux2-x86_64-standard:3.0")
+# linux_image("aws/codebuild/amazonlinux2-x86_64-standard:3.0")
+linux_image("aws/codebuild/standard:4.0")
 environment_variables(
   JETS_ENV: "test",
   # API_KEY: "ssm:/codebuild/demo/api_key" # Example of ssm parameter
