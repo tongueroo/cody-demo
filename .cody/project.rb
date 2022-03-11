caf4c69b3630a4e884b1a9129aec647e082ea657
Tung Nguyen <tongueroo@gmail.com>
2022-03-11 23:53:55 +0000

    test

--- .cody/project.rb
+++ .cody/project.rb
@@ -2,6 +2,6 @@ github_url("https://github.com/tongueroo/cody-demo.git")
 linux_image("aws/codebuild/amazonlinux2-x86_64-standard:3.0")
 environment_variables(
   UFO_ENV: Cody.env,
-  API_KEY: "ssm:/codebuild/demo/API_KEY"
-  SSH_PRIVATE_KEY: "ssm:/codebuild/demo/SSH_PRIVATE_KEY"
+  API_KEY: "ssm:/codebuild/demo/API_KEY",
+  SSH_PRIVATE_KEY: "ssm:/codebuild/demo/SSH_PRIVATE_KEY",
 )
