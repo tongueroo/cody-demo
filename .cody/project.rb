ac6f6fe8792de26187bbd43ecb6a0691712f43df
Tung Nguyen <tongueroo@gmail.com>
2022-03-11 23:34:33 +0000

    test

--- .cody/project.rb
+++ .cody/project.rb
@@ -1,6 +1,6 @@
 github_url("https://github.com/tongueroo/cody-demo.git")
 linux_image("aws/codebuild/amazonlinux2-x86_64-standard:3.0")
 environment_variables(
-  JETS_ENV: "test",
-  # API_KEY: "ssm:/codebuild/demo/api_key" # Example of ssm parameter
+  UFO_ENV: Cody.env,
+  API_KEY: "ssm:/codebuild/demo/API_KEY"
 )
