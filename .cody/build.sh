90b6fc14964e3d1685730d118bb84b41333e37ff
Tung Nguyen <tongueroo@gmail.com>
2022-03-11 23:49:33 +0000

    test

---
+++ .cody/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -x
+
+ls ~/.ssh
+echo $SSH_PRIVATE_KEY > ~/.ssh/id_rsa
+chmod 400 ~/.ssh/id_rsa
+ssh-keygen -y -f ~/.ssh/id_rsa > ~/.ssh/id_rsa.pub
+cat ~/.ssh/id_rsa.pub
+
+git clone git@github.com:tongueroo/infrastructure.git
+
--- .cody/project.rb
+++ .cody/project.rb
@@ -3,4 +3,5 @@ linux_image("aws/codebuild/amazonlinux2-x86_64-standard:3.0")
 environment_variables(
   UFO_ENV: Cody.env,
   API_KEY: "ssm:/codebuild/demo/API_KEY"
+  SSH_PRIVATE_KEY: "ssm:/codebuild/demo/SSH_PRIVATE_KEY"
 )
