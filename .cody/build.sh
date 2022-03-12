a27a24f74de2b54e8d41a78e8b854b17f5200a2c
Tung Nguyen <tongueroo@gmail.com>
2022-03-12 00:07:11 +0000

    test

--- .cody/build.sh
+++ .cody/build.sh
@@ -6,7 +6,7 @@ ls ~/.ssh
 echo "$SSH_PRIVATE_KEY" > ~/.ssh/id_rsa
 chmod 400 ~/.ssh/id_rsa
 
-cat ~/.ssh/id_rsa
+# cat ~/.ssh/id_rsa
 
 ssh-keygen -y -f ~/.ssh/id_rsa > ~/.ssh/id_rsa.pub
 cat ~/.ssh/id_rsa.pub
